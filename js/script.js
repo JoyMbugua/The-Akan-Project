73dc9d4d3de8cd1c163a1cec0b007861b68212d2
JoyMbugua <wacumbugua@gmail.com>
2020-10-05 22:51:30 +0300

    create allocation method for the week days

--- js/script.js
+++ js/script.js
@@ -10,4 +10,29 @@ function dayOfWeek (d, m, y) {
 
     let theDay = (Math.floor(( cc / 4) - 2 * cc - 1) + ((5 * yy) / 4) + (((mm + 1) * 26) / 10) + dd) % 7
      //0-6
+
+     if(theDay === 0) {
+        Day = "Sunday"
+    }
+     else if(theDay === 1) {
+        Day = "Monday"
+     }
+     else if(theDay=== 2) {
+        Day = "Tuesday"
+     }
+     else if(theDay === 3) {
+        Day = "Wednesday"
+     }
+     else if(theDay === 4) {
+        Day = "Thursday"
+     }
+     else if(theDay === 5) {
+        Day = "Friday"
+     }
+     else if(theDay === 6) {
+        Day = "Saturday"
+    }
+     else {
+        alert('seems something is wrong!') 
+     }
 }
