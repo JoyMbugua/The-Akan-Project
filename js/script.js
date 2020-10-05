83e147049ae35bc00ca02ca3314cde4219af3ea7
JoyMbugua <wacumbugua@gmail.com>
2020-10-05 22:49:43 +0300

    add the calculation

--- js/script.js
+++ js/script.js
@@ -8,4 +8,6 @@ function dayOfWeek (d, m, y) {
     let mm = parseInt(m)
     let dd = parseInt(d)
 
+    let theDay = (Math.floor(( cc / 4) - 2 * cc - 1) + ((5 * yy) / 4) + (((mm + 1) * 26) / 10) + dd) % 7
+     //0-6
 }
